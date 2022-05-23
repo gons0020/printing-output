19dcba4ae0c288249cffd6938681f8454699292d
gons0020 <105216576+gons0020@users.noreply.github.com>
2022-05-23 19:08:15 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,59 @@
+
+// TASK 1 
+
+let a=3;
+let b=5;
+let c=null;
+
+let result = 'let a = 3; \n';
+result+= 'let b = 5; \n';
+result+= 'let c= null; \n';
+result += '-------- \n';
+
+
+result += 'a + b =' + (a+b) + '\n';
+result += 'a - b =' + (a-b) + '\n';
+result += 'a * b =' + (a*b) + '\n';
+result += 'a / b =' + (a/b) + '\n';
+result += 'a % b =' + (a%b) + '\n';
+result += '(a +- b) :' + (a+-b) + '\n';
+result += '(a -= b) :' + (a-=b) + '\n';
+result += '( a*=b) :' + (a*=b) + '\n';
+result += '(a /= b) :' + (a/=b) + '\n';
+result += '(a %= b) :' + (a%=b) + '\n';
+result += '(a == b) :' + (a==b) + '\n';
+result += '(a !=b ) :' + (a!=b) + '\n';
+result += '(a > b) :' + (a>b) + '\n';
+result += '(a < b) :' + (a<b) + '\n';
+result += '(!a && !c):' + (!a && !c ) + '\n';
+result += '(!a || !c):' + (!a || !c) + '\n';
+
+alert (result);
+
+// TASK 2 
+
+let first_name = 'Krisanne';
+let last_name = 'Gonsalves';
+let email = 'gons0020@algonquinlive.com'
+let output; 
+
+output = 'My name is' + ' ' + first_name + ' ' + last_name + '. '  + 'You can contact me at' +  ' '  + email + '.';
+
+alert (output);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
